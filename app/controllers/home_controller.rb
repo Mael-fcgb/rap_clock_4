e300c1aafa3b3bd8f4aa43ac7f2584b5f57d20c7
baptwobapt <bapt3380@gmail.com>
2025-02-21 17:14:14 +0100

    home css 2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     # Récupérer tous les projets triés par date de manière décroissante
-    @projects = Project.all.order(date: :desc)
+    @projects = Project.all.order(date: :asc)
 
     # Regrouper les projets par date
     @projects_by_date = @projects.group_by(&:date)
